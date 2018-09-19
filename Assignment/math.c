83fcf07eb1c5bdf7128ff42aabd5711355d93e53
davidrusso2020 <36418482+davidrusso2020@users.noreply.github.com>
2018-09-19 11:47:48 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -8,39 +8,9 @@
  *       Author : David Russo
  *
  ******************************************************************
- *
- *
- * math.c is a basic calculator program that takes two integers, an
- * operator, and returns the result of the operation
- *
- * Parameters:
- *   - 2 integers: num1, num2 (int)
- *   - 1 arithmetic/bitwise logic operator: Operator (char)
- *
- * math.c contains 11 valid arithmetic/bitwise logic functions
- *
- * Below is a list of all valid chars for Operator, what each char
- * means, and the mathematical representation of the operation
- * '+' :    Add            (num1 + num2)
- * '-' :    Subtract       (num1 - num2)
- * '*' :    Multiply       (num1 * num2)
- * '/' :    Divide         (num1 / num2)
- * '%' :    Modulus        (num1 % num2)
- * '<' :    Left Shift     (num1 << num2)
- * '>' :    Right Shift    (num1 >> num2)
- * '&' :    Bitwise AND    (num1 & num2)
- * '|' :    Bitwise OR     (num1 | num2)
- * '^' :    Bitwise XOR    (num1 ^ num2)
- * '~' :    Bitwise NOT    (~num1) *for this operation, num1 and num2
- *                                  are still needed*
- *
- * NOTE: because of the nature of integer data types, information
- * may be lost due to truncation when the result of num1/num2 is not an
- * integer
- *
- ************************************************************************
-*/
-
+ */
+// This function uses a switch statement based on the Operator to
+// decide what the result of the function is
 int math(int num1, int num2, char Operator)
 {
     switch(Operator)
